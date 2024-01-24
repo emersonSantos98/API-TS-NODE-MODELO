470250bab009fe1e8aeba5c63a4500a1e9bef121
Emerson <emerson.matosokl10@gmail.com>
2024-01-24 03:13:29 -0300

    feat: lib para validação de dados "Yup" data:  24/01/2024

--- src/controllers/usuarios/create.ts
+++ src/controllers/usuarios/create.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from 'express';
 import {StatusCodes} from 'http-status-codes';
 import * as yup from 'yup';
-//
+
 interface IUsuario {
   nome: string;
   email: string;
